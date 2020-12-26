768cb6e2222c1a60f44d5aaf02925a52f3e923b1
Abhishek Singh <abhishek.viki@gmail.com>
2020-12-27 00:27:58 +0530

    39_Default Profile

--- src/main/java/in/abhisingh/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/in/abhisingh/sfgdi/services/I18nSpanishGreetingService.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service;
 
 //We need to setup the active profile in application.properties to remove the ambiguity.
 // Spring will simply ignore and not load the inactive beans.
-@Profile("ES")
+//@Profile("ES")
+
+//In this way we can setup our bean for multiple profiles. In this case for ES and default profile.
+//Upon setting up the default profile to a bean we don't need to setup active profile in application.properties.
+@Profile({"ES", "default"})
 //This is also a method to setup qualifiers string to make it something other than the class name with first lower case letter.
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
